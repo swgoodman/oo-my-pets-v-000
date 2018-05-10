01fa1f0300454bb524fbc761ee418a5256f8c183
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-10 21:02:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,7 +66,7 @@ class Owner
   end
 
   def sell_pets
-    @pets.update(@pets) { |key, value| key.mood = "nervous" }
+    hash.update(@pets) { |key, value| key.mood = "nervous" }
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
